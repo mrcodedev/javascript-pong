1208bc44527b6bbdd44e9c3a3f7fe467809b8f94
mrcodedev <mrcodedev@gmail.com>
2019-02-05 17:48:57 +0100

    Refactoring with ES6 and adding Classes

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # jpong
 JS Pong
+
+Siguiendo el tutorial de YouTube https://www.youtube.com/watch?v=ju09womACpQ
+
+Es curiosidad sana de ver como se hace un simple pong con **Canvas** y **JavaScript**.
--- js/pong.js
+++ js/pong.js
@@ -12,6 +12,22 @@ class Rect {
         this.pos = new Vec;
         this.size = new Vec(w, h);
     }
+
+    get left() {
+        return this.pos.x - this.size.x / 2;
+    }
+
+    get right() {
+        return this.pos.x + this.size.x / 2;
+    }
+
+    get top() {
+        return this.pos.y - this.size.y / 2;
+    }
+
+    get bottom() {
+        return this.pos.y + this.size.y / 2;
+    }
 }
 
 class Ball extends Rect {
@@ -22,58 +38,64 @@ class Ball extends Rect {
     }
 }
 
+class Pong {
+    constructor(canvas) {
+        this._canvas = canvas;
+        this._context = canvas.getContext('2d');
+
+        //Creamos una nueva bola
+        this.ball = new Ball;
+        //La colocamos
+        this.ball.pos.x = 100;
+        this.ball.pos.y = 50;
+
+        //Le decimos la velocidad en X e Y
+        this.ball.vel.x = 100;
+        this.ball.vel.y = 100;
+
+        //Ultimo movimiento
+        let lastTime;
+
+        const callback = (millis) => {
+            //Si existe actualizamos
+            if (lastTime) {
+                //Fotogramas que se crean
+                this.update((millis - lastTime) / 1000);
+            }
+
+            lastTime = millis;
+            //Creamos un nuevo fotograma
+            requestAnimationFrame(callback);
+        }
+        callback();
+    }
+
+    //Para animar la pelota con un dt (Delta Time)
+    update(dt) {
+        //Hacemos que la pelota actualice su posicion
+        this.ball.pos.x += this.ball.vel.x * dt;
+        this.ball.pos.y += this.ball.vel.y * dt;
+    
+        //Vemos si se sale de la pantalla e invertimos la velocidad por width
+        if(this.ball.left < 0 || this.ball.right > this._canvas.width) {
+            this.ball.vel.x = -this.ball.vel.x;
+        }
+    
+        //Vemos si se sale de la pantalla e invertimos la velocidad por height
+        if(this.ball.top < 0 || this.ball.bottom > this._canvas.height) {
+            this.ball.vel.y = -this.ball.vel.y;
+        }
+    
+        //Dibujamos el escenario
+        this._context.fillStyle = "#000";
+        this._context.fillRect(0, 0, this._canvas.width, this._canvas.height);
+    
+        //Dibujamos la pelota
+        this._context.fillStyle = "#fff";
+        this._context.fillRect(this.ball.pos.x, this.ball.pos.y, this.ball.size.x, this.ball.size.y);
+    }
+}
+
 //Creamos el canvas
 const canvas = document.getElementById('pong');
-const context = canvas.getContext('2d');
-
-//Creamos una nueva bola
-const ball = new Ball;
-//La colocamos
-ball.pos.x = 100;
-ball.pos.y = 50;
-
-//Le decimos la velocidad en X e Y
-ball.vel.x = 100;
-ball.vel.y = 100;
-
-//Ultimo movimiento
-let lastTime;
-
-function callback(millis) {
-    //Si existe actualizamos
-    if (lastTime) {
-        //Fotogramas que se crean
-        update((millis - lastTime) / 1000);
-    }
-
-    lastTime = millis;
-    //Creamos un nuevo fotograma
-    requestAnimationFrame(callback);
-}
-
-//Para animar la pelota con un dt (Delta Time)
-function update(dt) {
-    //Hacemos que la pelota actualice su posicion
-    ball.pos.x += ball.vel.x * dt;
-    ball.pos.y += ball.vel.y * dt;
-
-    //Vemos si se sale de la pantalla e invertimos la velocidad por width
-    if(ball.pos.x < 0 || ball.pos.x > canvas.width) {
-        ball.vel.x = -ball.vel.x;
-    }
-
-    //Vemos si se sale de la pantalla e invertimos la velocidad por height
-    if(ball.pos.y < 0 || ball.pos.y > canvas.height) {
-        ball.vel.y = -ball.vel.y;
-    }
-
-    //Dibujamos el escenario
-    context.fillStyle = "#000";
-    context.fillRect(0, 0, canvas.width, canvas.height);
-
-    //Dibujamos la pelota
-    context.fillStyle = "#fff";
-    context.fillRect(ball.pos.x, ball.pos.y, ball.size.x, ball.size.y);
-}
-
-callback();
+const pong = new Pong(canvas);
