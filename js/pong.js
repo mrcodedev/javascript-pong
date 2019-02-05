4351a7a2e7a80a217c2763458c6693d74f0586ff
mrcodedev <mrcodedev@gmail.com>
2019-02-05 17:30:01 +0100

    Adding all comments of all lines

--- js/pong.js
+++ js/pong.js
@@ -1,3 +1,4 @@
+//Clase vector para las posiciones
 class Vec {
     constructor (x=0, y=0) {
         this.x = x;
@@ -5,6 +6,7 @@ class Vec {
     }
 }
 
+//Para crear un rectangulo
 class Rect {
     constructor(w, h) {
         this.pos = new Vec;
@@ -14,6 +16,7 @@ class Rect {
 
 class Ball extends Rect {
     constructor() {
+        //Marcamos el tamaño de la bola de 10, 10
         super(10, 10);
         this.vel = new Vec;
     }
@@ -23,28 +26,46 @@ class Ball extends Rect {
 const canvas = document.getElementById('pong');
 const context = canvas.getContext('2d');
 
+//Creamos una nueva bola
 const ball = new Ball;
+//La colocamos
 ball.pos.x = 100;
 ball.pos.y = 50;
 
+//Le decimos la velocidad en X e Y
 ball.vel.x = 100;
 ball.vel.y = 100;
 
+//Ultimo movimiento
 let lastTime;
+
 function callback(millis) {
+    //Si existe actualizamos
     if (lastTime) {
+        //Fotogramas que se crean
         update((millis - lastTime) / 1000);
     }
 
     lastTime = millis;
+    //Creamos un nuevo fotograma
     requestAnimationFrame(callback);
 }
 
+//Para animar la pelota con un dt (Delta Time)
 function update(dt) {
-    //Hacemos que la pelota tome velocidad y se mueva
+    //Hacemos que la pelota actualice su posicion
     ball.pos.x += ball.vel.x * dt;
     ball.pos.y += ball.vel.y * dt;
 
+    //Vemos si se sale de la pantalla e invertimos la velocidad por width
+    if(ball.pos.x < 0 || ball.pos.x > canvas.width) {
+        ball.vel.x = -ball.vel.x;
+    }
+
+    //Vemos si se sale de la pantalla e invertimos la velocidad por height
+    if(ball.pos.y < 0 || ball.pos.y > canvas.height) {
+        ball.vel.y = -ball.vel.y;
+    }
 
     //Dibujamos el escenario
     context.fillStyle = "#000";
